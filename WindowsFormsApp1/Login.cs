11b3b631aacab836c6f63e9be9e3ad90032e0041
Nikola Grudev <nikcho412@gmail.com>
2024-03-15 09:01:23 +0200

    nikol

--- WindowsFormsApp1/Login.cs
+++ WindowsFormsApp1/Login.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
 
 namespace WindowsFormsApp1
 {
@@ -24,17 +27,7 @@ namespace WindowsFormsApp1
 
         public void button1_Click(object sender, EventArgs e)
         {
-            // Check the login credentials or condition for determining the user type
-            if (true) // normal hashed MD5 login
-            {
-                work.Show();               
-                this.Hide();
-            }
-            else if (false) // implement Admin login
-            {
-                adminForm.Show();
-                this.Hide();
-            }
+           
         }
 
         private void Form1_Load(object sender, EventArgs e)
